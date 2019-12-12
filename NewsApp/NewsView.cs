8bb0dbe6543f2857cbd81934dce7138a3bac8fea
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-12 22:56:09 +0900

    [CREATE] NewsView

---
+++ NewsApp/NewsView.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace NewsApp
+{
+  public partial class NewsView : Form
+  {
+    private Session sess;
+    
+    public NewsView(Session sess)
+    {
+      InitializeComponent();
+      this.sess = sess;
+    }
+  }
+}
