bd066fd698a78a24fdecb9712ed4a25e2c2d456a
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-12 20:47:07 +0900

    [CREATE] Main Page UI

---
+++ NewsApp/MainForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace NewsApp
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources =
+        new System.ComponentModel.ComponentResourceManager(typeof(NewsApp.MainForm));
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+      this.pictureBox1.Location = new System.Drawing.Point(202, 99);
+      this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(413, 142);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+      this.pictureBox1.TabIndex = 0;
+      this.pictureBox1.TabStop = false;
+      this.label1.Font = new System.Drawing.Font("맑은 고딕", 20F);
+      this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+      this.label1.Location = new System.Drawing.Point(317, 242);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(179, 59);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "뉴스";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Items.AddRange(new object[] {"로그인", "회원가입"});
+      this.comboBox1.Location = new System.Drawing.Point(294, 372);
+      this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(229, 23);
+      this.comboBox1.TabIndex = 2;
+      this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+      this.label2.Location = new System.Drawing.Point(294, 408);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(58, 31);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "아이디";
+      this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.textBox1.Location = new System.Drawing.Point(352, 412);
+      this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(171, 23);
+      this.textBox1.TabIndex = 4;
+      this.button1.Location = new System.Drawing.Point(294, 452);
+      this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(228, 28);
+      this.button1.TabIndex = 5;
+      this.button1.Text = "로그인";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.White;
+      this.ClientSize = new System.Drawing.Size(800, 635);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.comboBox1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.pictureBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.Name = "MainForm";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Definately Not a NAVER News";
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+  }
+}
---
+++ NewsApp/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewsApp
+{
+  public partial class MainForm : Form
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      // TODO: DB 연결해서 회원가입 / 로그인
+    }
+
+    private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+    }
+  }
+}
