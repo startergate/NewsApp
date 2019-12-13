6284cc476cd77a3acb8f1345af3a73a3ad516ae1
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-14 03:08:46 +0900

    [UPDATE] DataGridView at NewsView Layout

--- NewsApp/NewsView.Designer.cs
+++ NewsApp/NewsView.Designer.cs
@@ -37,12 +37,14 @@ namespace NewsApp
       // 
       // dataGridView1
       // 
-      this.dataGridView1.ColumnHeadersHeightSizeMode =
-        System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
       this.dataGridView1.Location = new System.Drawing.Point(11, 8);
       this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.dataGridView1.MultiSelect = false;
       this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.ReadOnly = true;
       this.dataGridView1.RowTemplate.Height = 23;
+      this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
       this.dataGridView1.Size = new System.Drawing.Size(777, 548);
       this.dataGridView1.TabIndex = 0;
       this.dataGridView1.CellDoubleClick +=
