08f958ceb244b28b9481f2159b8487bde03e20af
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-14 02:34:54 +0900

    [UPDATE] News View UI

--- NewsApp/NewsView.Designer.cs
+++ NewsApp/NewsView.Designer.cs
@@ -31,19 +31,39 @@ namespace NewsApp
     /// </summary>
     private void InitializeComponent()
     {
+      this.dataGridView1 = new System.Windows.Forms.DataGridView();
+      ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
       this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
+      this.dataGridView1.ColumnHeadersHeightSizeMode =
+        System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.dataGridView1.Location = new System.Drawing.Point(11, 8);
+      this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.dataGridView1.Name = "dataGridView1";
+      this.dataGridView1.RowTemplate.Height = 23;
+      this.dataGridView1.Size = new System.Drawing.Size(777, 548);
+      this.dataGridView1.TabIndex = 0;
+      this.dataGridView1.CellDoubleClick +=
+        new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
       // 
       // NewsView
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 562);
+      this.Controls.Add(this.dataGridView1);
       this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
       this.Name = "NewsView";
       this.Text = "NewsView";
+      this.Load += new System.EventHandler(this.NewsView_Load);
+      ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
       this.ResumeLayout(false);
     }
 
     #endregion
+
+    private System.Windows.Forms.DataGridView dataGridView1;
   }
 }
