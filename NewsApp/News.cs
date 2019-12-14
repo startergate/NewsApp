a0856606c83a7c11142fb74b9fdb5467e1139ad2
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-15 03:04:17 +0900

    [CREATE] News Structure

---
+++ NewsApp/News.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace NewsApp
+{
+  public struct News
+  {
+    public string title;
+    public string content;
+    public int pressid;
+    public int rptid;
+    public string press_name;
+    public string rpt_name;
+  }
+}
