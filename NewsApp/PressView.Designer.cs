ffe88ad220327c5297ac2e0eb4b27b3b97492f57
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-12-15 07:51:37 +0900

    [UPDATE] Adjust Indent

--- NewsApp/PressView.Designer.cs
+++ NewsApp/PressView.Designer.cs
@@ -34,6 +34,9 @@ namespace NewsApp
       this.dataGridView1 = new System.Windows.Forms.DataGridView();
       ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
       this.SuspendLayout();
+      // 
+      // dataGridView1
+      // 
       this.dataGridView1.ColumnHeadersHeightSizeMode =
         System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
       this.dataGridView1.Location = new System.Drawing.Point(12, 12);
@@ -42,6 +45,9 @@ namespace NewsApp
       this.dataGridView1.RowTemplate.Height = 23;
       this.dataGridView1.Size = new System.Drawing.Size(776, 538);
       this.dataGridView1.TabIndex = 1;
+      // 
+      // PressView
+      // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 562);
